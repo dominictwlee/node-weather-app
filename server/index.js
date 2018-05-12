cb2a4b4eb4cfb8310f3396cf9cbec6259207f239
Dominic Lee <34794189+dominictwlee@users.noreply.github.com>
2018-05-13 00:51:45 +0100

    Configure for production

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 require('dotenv').config();
 
 const weatherRouter = require('./weather/weather.route.js');
 
 const app = express();
+
 app.use(cors());
+
+app.use(express.static(path.join(__dirname, 'client/dist')));
 
 app.use('/api/weather', weatherRouter);
 
