9ffdfce551dcb02391d4aaf89426bbe588bcfc83
Dominic Lee <34794189+dominictwlee@users.noreply.github.com>
2018-05-13 01:43:24 +0100

    Route react html file to catch-all handler

--- server/index.js
+++ server/index.js
@@ -13,6 +13,10 @@ app.use(express.static(path.join(__dirname, 'client/dist')));
 
 app.use('/api/weather', weatherRouter);
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/dist/index.html'));
+});
+
 app.listen(process.env.PORT, () => console.log(`Server started on 3000`));
 
 module.exports = app;
