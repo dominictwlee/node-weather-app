eb98eef272e23cb6ff62e8f77a1f314910654a95
Dominic Lee <34794189+dominictwlee@users.noreply.github.com>
2018-05-13 10:13:12 +0100

    Require config file for env

--- server/index.js
+++ server/index.js
@@ -3,18 +3,19 @@ const path = require('path');
 const cors = require('cors');
 require('dotenv').config();
 
+require('./config');
 const weatherRouter = require('./weather/weather.route.js');
 
 const app = express();
 
 app.use(cors());
 
-app.use(express.static(path.join(`${__dirname}/client/dist`)));
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.use('/api/weather', weatherRouter);
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(`${__dirname}/client/dist/index.html`));
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
 app.listen(process.env.PORT, () => console.log(`Server started on 3000`));
