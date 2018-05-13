44f6b52a783f10196ae3ae38a61c09ea11d91056
Dominic Lee <34794189+dominictwlee@users.noreply.github.com>
2018-05-13 01:48:47 +0100

    Fix path

--- server/index.js
+++ server/index.js
@@ -9,12 +9,12 @@ const app = express();
 
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, 'client/dist')));
+app.use(express.static(path.join(__dirname, '/client/dist')));
 
 app.use('/api/weather', weatherRouter);
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/dist/index.html'));
+  res.sendFile(path.join(__dirname, '/client/dist/index.html'));
 });
 
 app.listen(process.env.PORT, () => console.log(`Server started on 3000`));
